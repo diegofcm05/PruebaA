c0bd3bb327a396fdd4166772d2b6297a8413aa15
Diego Cruz <dfcm950@gmail.com>
2023-08-11 06:40:54 -0600

    Primer commit

---
+++ src/pruebaa/PruebaA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaa;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class PruebaA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola como estan");
+        System.out.println("Soy Diego");
+        // TODO code application logic here
+    }
+    
+}
